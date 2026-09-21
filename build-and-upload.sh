596c3f99599e821c3c484320b374fea3667c08a6
ethanhaoyicheng <ethanhaoyicheng@gmail.com>
2026-09-21 07:38:55 -0700

    removed mac ver 1 and shipped ver 2 (although this one has wrong main.spec)

---
+++ build-and-upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo "Building Evolution Simulator..."
+rm -rf build
+python3.11 -m PyInstaller main.spec
+
+echo "Uploading to itch.io..."
+/Applications/butler push \
+"dist/Evolution Simulator.app" \
+twinkledelux/evosim:mac
+
+echo "Done!"
+
